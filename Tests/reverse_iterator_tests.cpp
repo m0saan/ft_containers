33f47203b7dfc9d6facc731ec338ab9699cf0ebe
MoBoustta <moboustta6@gmail.com>
2021-08-08 21:00:54 +0100

    Testing the implementation of the iterators

---
+++ Tests/reverse_iterator_tests.cpp
@@ -0,0 +1,33 @@
+//
+// Created by moboustt on 8/8/2021.
+//
+
+// reverse_iterator example
+#include <iostream>     // std::cout
+#include <vector>       // std::vector
+#include <iterator>
+#include "../Utility/reverse_iterator.hpp"
+
+int main () {
+	std::vector<int> myvector;
+	for (int i=0; i<10; i++)
+		myvector.push_back(i);
+
+	typedef std::vector<int>::iterator iter_type;
+	// ? 0 1 2 3 4 5 6 7 8 9 ?
+	iter_type from (myvector.begin());                     //   ^
+	//         ------>
+	iter_type until (myvector.end());                      //                       ^
+	//
+	ft::reverse_iterator<iter_type> rev_until (from);     // ^
+	//         <------
+	ft::reverse_iterator<iter_type> rev_from (until);     //
+
+	std::cout << "myvector:";
+	while (rev_from != rev_until)
+		std::cout << ' ' << *rev_from++;
+	std::cout << '\n';
+
+	return 0;
+}
+
