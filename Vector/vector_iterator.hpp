4350968c36331e04a2f23a5a417fb1ef0916779a
MoBoustta <moboustta6@gmail.com>
2021-08-10 02:06:31 +0100

    fix the diference:type warning for vector_iterator

--- Vector/vector_iterator.hpp
+++ Vector/vector_iterator.hpp
@@ -15,12 +15,7 @@ namespace ft {
 
 	public:
 
-		typedef Iterator iterator_type;
-		typedef typename iterator_traits<Iterator>::iterator_category iterator_category;
-		typedef typename iterator_traits<Iterator>::value_type value_type;
-		typedef typename iterator_traits<Iterator>::difference_type difference_type;
-		typedef typename iterator_traits<Iterator>::pointer pointer;
-		typedef typename iterator_traits<Iterator>::reference reference;
+		typedef ptrdiff_t difference_type;
 
 		/*
 		 * Constructor & Copy assignment operator.
