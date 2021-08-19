b449889b8c94464de3051b3bbb038856dbab58d1
MoBoustta <moboustta6@gmail.com>
2021-08-19 03:36:25 +0100

    implement the stack data structure

---
+++ Stack/stack.cpp
@@ -0,0 +1,67 @@
+//
+// Created by moboustt on 19/8/2021.
+//
+
+#include "stack.hpp"
+#include <stack>
+
+int main() {
+
+	std::cout << "********************* <Testing Stack Constructors *********************" << std::endl;
+	{
+		ft::stack<int> c1;
+		c1.push(5);
+		std::cout << c1.size() << '\n';
+
+		ft::stack<int> c2(c1);
+		std::cout << c2.size() << '\n';
+
+		std::vector<int> vec {3, 1, 4, 1, 5};
+		ft::stack<int> c3(vec);
+		std::cout << c3.size() << '\n';
+	}
+
+	std::cout << "********************* <Testing Top() method. *********************" << std::endl;
+	{
+		std::stack<int>   s;
+
+		s.push( 2 );
+		s.push( 6 );
+		s.push( 51 );
+
+		std::cout << s.size() << " elements on stack\n";
+		std::cout << "Top element: "
+		<< s.top()         // Leaves element on stack
+		<< "\n";
+		std::cout << s.size() << " elements on stack\n";
+		s.pop();
+		std::cout << s.size() << " elements on stack\n";
+		std::cout << "Top element: " << s.top() << "\n";
+	}
+
+	std::cout << "********************* <Testing empty() method.> *********************" << std::endl;
+	{
+		std::cout << std::boolalpha;
+
+		std::stack<int> container;
+
+		std::cout << "Initially, container.empty(): " << container.empty() << '\n';
+
+		container.push(42);
+		std::cout << "After adding elements, container.empty(): " << container.empty() << '\n';
+	}
+
+	std::cout << "********************* <Testing size() method.> *********************" << std::endl;
+	{
+		std::cout << std::boolalpha;
+
+		std::stack<int> container;
+
+		std::cout << "Initially, container.size(): " << container.size() << '\n';
+
+		for (int i = 0; i < 7; ++i)
+			container.push(i);
+		std::cout << "After adding elements, container.size(): " << container.size() << '\n';
+	}
+	return 0;
+}
---
+++ Stack/stack.hpp
@@ -0,0 +1,125 @@
+//
+// Created by moboustt on 19/8/2021.
+//
+
+#ifndef __STACK_HPP__
+#define __STACK_HPP__
+
+#include "../Vector/vector.hpp"
+
+namespace ft {
+
+	template<typename T, class Container = ft::vector<T> >
+	class stack {
+
+	public:
+
+		typedef Container container_type;
+		typedef typename Container::value_type value_type;
+		typedef typename Container::size_type size_type;
+		typedef typename Container::reference reference;
+		typedef typename Container::const_reference const_reference;
+
+	public:
+
+		explicit stack(const Container &cont = Container()) : _c(cont) {}
+
+		stack(const stack &other) { *this = other; }
+
+		template<class InputIt>
+		stack(InputIt first, InputIt last): _c(first, last) {}
+
+		template<class InputIt, class Alloc>
+		stack(InputIt first, InputIt last, const Alloc &alloc) : _c(first, last, alloc) {}
+
+		~stack() {}
+
+		stack &operator=(const stack &other) {
+			if(*this != other)
+				_c = other._c;
+			return *this;
+		}
+
+		// -*-  Element access  -*-
+
+		reference top() {
+			return _c.back();
+		}
+
+		const_reference top() const {
+			return _c.back();
+		}
+
+		// -*-  Capacity  -*-
+
+		bool empty() const {
+			return _c.empty();
+		}
+
+		size_type size() const {
+			return _c.size();
+		}
+
+
+		// -*-  Modifiers  -*-
+
+		void push(const value_type &value) {
+			_c.push_back(value);
+		}
+
+		void pop() {
+			_c.pop_back();
+		}
+
+		// -*-  Non-member functions  -*-
+		friend bool operator==(const stack &lhs, const stack &rhs);
+
+		friend bool operator!=(const stack &lhs, const stack &rhs);
+
+		friend bool operator<(const stack &lhs, const stack &rhs);
+
+		friend bool operator<=(const stack &lhs, const stack &rhs);
+
+		friend bool operator>(const stack &lhs, const stack &rhs);
+
+		friend bool operator>=(const stack &lhs, const stack &rhs);
+
+	protected:
+
+		container_type _c;
+	};
+
+	template<class T, class Container>
+	bool operator==(const stack<T, Container> &lhs, const stack<T, Container> &rhs) {
+		return lhs._c == rhs._c;
+	}
+
+	template<class T, class Container>
+	bool operator!=(const stack<T, Container> &lhs, const stack<T, Container> &rhs) {
+		return !(lhs == rhs);
+	}
+
+	template<class T, class Container>
+	bool operator<(const stack<T, Container> &lhs, const stack<T, Container> &rhs) {
+		return lhs._c < rhs._c;
+	}
+
+	template<class T, class Container>
+	bool operator<=(const stack<T, Container> &lhs, const stack<T, Container> &rhs) {
+		return lhs._c <= rhs._c;
+	}
+
+	template<class T, class Container>
+	bool operator>(const stack<T, Container> &lhs, const stack<T, Container> &rhs) {
+		return !(lhs > rhs);
+	}
+
+	template<class T, class Container>
+	bool operator>=(const stack<T, Container> &lhs, const stack<T, Container> &rhs) {
+		return !(lhs <= rhs);
+	}
+
+}
+
+
+#endif // __STACK_HPP__
