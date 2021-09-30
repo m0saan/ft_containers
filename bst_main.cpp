6b2103f0373c33182bf1f0664e2a7b5f24009d9c
mo-san99 <moboustta6@gmail.com>
2021-09-30 13:07:20 +0100

    testing the remove logic

--- bst_main.cpp
+++ bst_main.cpp
@@ -9,25 +9,25 @@
 
 #define TestPair 0
 
-template<typename T1, class T2>
-std::ostream &operator<<(std::ostream &os, ft::pair<T1, T2> const &p) {
-    os << '(' << p.first << ", " << p.second << ')' << std::endl;
-    return os;
-}
+// template<typename T1, class T2>
+// std::ostream &operator<<(std::ostream &os, ft::pair<T1, T2> const &p) {
+//     os << '(' << p.first << ", " << p.second << ')' << std::endl;
+//     return os;
+// }
 
-int main() {
-
+int main()
+{
 
 #if TestPair
     std::cout << "*------------------- Testing pair -------------------*" << std::endl;
-    ft::pair <std::string,double> product1;                     // default constructor
-    ft::pair <std::string,double> product2 ("tomatoes",2.30);   // value init
-    ft::pair <std::string,double> product3 (product2);          // copy constructor
+    ft::pair<std::string, double> product1;                   // default constructor
+    ft::pair<std::string, double> product2("tomatoes", 2.30); // value init
+    ft::pair<std::string, double> product3(product2);         // copy constructor
 
-    product1 = ft::make_pair(std::string("lightbulbs"),0.99);   // using make_pair (move)
+    product1 = ft::make_pair(std::string("lightbulbs"), 0.99); // using make_pair (move)
 
-    product2.first = "shoes";                  // the type of first is string
-    product2.second = 39.90;                   // the type of second is double
+    product2.first = "shoes"; // the type of first is string
+    product2.second = 39.90;  // the type of second is double
 
     std::cout << "The price of " << product1.first << " is $" << product1.second << '\n';
     std::cout << "The price of " << product2.first << " is $" << product2.second << '\n';
@@ -36,9 +36,9 @@ int main() {
     std::cout << std::endl;
     std::cout << "-----------------------------------" << std::endl;
 
-    ft::pair <std::string,int> planet, homeplanet;
+    ft::pair<std::string, int> planet, homeplanet;
 
-    planet = ft::make_pair("Earth",6371);
+    planet = ft::make_pair("Earth", 6371);
 
     homeplanet = planet;
 
@@ -48,22 +48,27 @@ int main() {
     std::cout << std::endl;
     std::cout << "-----------------------------------" << std::endl;
 
-    ft::pair<int,char> foo (10,'z');
-    ft::pair<int,char> bar (90,'a');
+    ft::pair<int, char> foo(10, 'z');
+    ft::pair<int, char> bar(90, 'a');
 
-    if (foo==bar) std::cout << "foo and bar are equal\n";
-    if (foo!=bar) std::cout << "foo and bar are not equal\n";
-    if (foo< bar) std::cout << "foo is less than bar\n";
-    if (foo> bar) std::cout << "foo is greater than bar\n";
-    if (foo<=bar) std::cout << "foo is less than or equal to bar\n";
-    if (foo>=bar) std::cout << "foo is greater than or equal to bar\n";
+    if (foo == bar)
+        std::cout << "foo and bar are equal\n";
+    if (foo != bar)
+        std::cout << "foo and bar are not equal\n";
+    if (foo < bar)
+        std::cout << "foo is less than bar\n";
+    if (foo > bar)
+        std::cout << "foo is greater than bar\n";
+    if (foo <= bar)
+        std::cout << "foo is less than or equal to bar\n";
+    if (foo >= bar)
+        std::cout << "foo is greater than or equal to bar\n";
 
+    ft::pair<int, int> fooo;
+    ft::pair<int, int> barr;
 
-    ft::pair <int,int> fooo;
-    ft::pair <int,int> barr;
-
-    fooo = ft::make_pair (10,20);
-    barr = ft::make_pair (10.5,'A'); // ok: implicit conversion from pair<double,char>
+    fooo = ft::make_pair(10, 20);
+    barr = ft::make_pair(10.5, 'A'); // ok: implicit conversion from pair<double,char>
 
     std::cout << "fooo: " << fooo.first << ", " << fooo.second << '\n';
     std::cout << "barr: " << barr.first << ", " << barr.second << '\n';
@@ -78,14 +83,21 @@ int main() {
         avlTree.insert(ft::make_pair(i, i * 2));
 
     avlTree.remove(*avlTree.begin());
-    avlTree.remove(*avlTree.begin());
-    avlTree.remove(*avlTree.begin());
+    // avlTree.remove(ft::make_pair(3,6));
+    // avlTree.remove(ft::make_pair(5,10));
+    avlTree.remove(ft::make_pair(9,18));
+    avlTree.remove(ft::make_pair(10,20));
 
-    for (AVL_INT_INT::iterator it = avlTree.begin(); it != avlTree.end(); ++it) {
-        std::cout << *it;
-    }
+    std::cout << "------------------------------------" << std::endl;
+      for (AVL_INT_INT::iterator it = avlTree.begin(); it != avlTree.end(); ++it)
+        std::cout << *it << std::endl;
+    std::cout << "------------------------------------" << std::endl;
 
-    // avlTree.inOrderTraversal();
+    std::cout << "------------------------------------" << std::endl;
+      for (AVL_INT_INT::reverse_iterator r_it = avlTree.rbegin(); r_it != avlTree.rend(); ++r_it)
+        std::cout << *r_it << std::endl;
+    std::cout << "------------------------------------" << std::endl;
+
 
     return 0;
 }
