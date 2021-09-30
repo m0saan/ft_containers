12e8ce8cb0c6094e03905dff6cc59e92dbffbfd4
mo-san99 <moboustta6@gmail.com>
2021-09-30 14:19:35 +0100

    bst tests

--- bst_main.cpp
+++ bst_main.cpp
@@ -7,6 +7,24 @@
 #include <string>
 #include <map>
 
+#define RESET "\033[0m"
+#define BLACK "\033[30m"              /* Black */
+#define RED "\033[31m"                /* Red */
+#define GREEN "\033[32m"              /* Green */
+#define YELLOW "\033[33m"             /* Yellow */
+#define BLUE "\033[34m"               /* Blue */
+#define MAGENTA "\033[35m"            /* Magenta */
+#define CYAN "\033[36m"               /* Cyan */
+#define WHITE "\033[37m"              /* White */
+#define BOLDBLACK "\033[1m\033[30m"   /* Bold Black */
+#define BOLDRED "\033[1m\033[31m"     /* Bold Red */
+#define BOLDGREEN "\033[1m\033[32m"   /* Bold Green */
+#define BOLDYELLOW "\033[1m\033[33m"  /* Bold Yellow */
+#define BOLDBLUE "\033[1m\033[34m"    /* Bold Blue */
+#define BOLDMAGENTA "\033[1m\033[35m" /* Bold Magenta */
+#define BOLDCYAN "\033[1m\033[36m"    /* Bold Cyan */
+#define BOLDWHITE "\033[1m\033[37m"   /* Bold White */
+
 #define TestPair 0
 
 // template<typename T1, class T2>
@@ -14,6 +32,20 @@
 //     os << '(' << p.first << ", " << p.second << ')' << std::endl;
 //     return os;
 // }
+
+template <typename T1, typename T2>
+void insert(T1 &avl, T2 const &input, int size)
+{
+    for (size_t i = 0; i < size; i++)
+        avl.insert(ft::make_pair(input[i], input[i] * 2));
+}
+
+template <typename Iter>
+void put(Iter begin, Iter end)
+{
+    for (; begin != end; begin++)
+        std::cout << '\t' << *begin << std::endl;
+}
 
 int main()
 {
@@ -73,10 +105,6 @@ int main()
     std::cout << "fooo: " << fooo.first << ", " << fooo.second << '\n';
     std::cout << "barr: " << barr.first << ", " << barr.second << '\n';
 
-#endif
-
-    typedef ft::AVLTree<ft::pair<int, int> > AVL_INT_INT;
-
     ft::AVLTree<ft::pair<int, int> > avlTree;
 
     for (int i = 1; i <= 10; ++i)
@@ -85,19 +113,49 @@ int main()
     avlTree.remove(*avlTree.begin());
     // avlTree.remove(ft::make_pair(3,6));
     // avlTree.remove(ft::make_pair(5,10));
-    avlTree.remove(ft::make_pair(9,18));
-    avlTree.remove(ft::make_pair(10,20));
+    avlTree.remove(ft::make_pair(9, 18));
+    avlTree.remove(ft::make_pair(10, 20));
 
     std::cout << "------------------------------------" << std::endl;
-      for (AVL_INT_INT::iterator it = avlTree.begin(); it != avlTree.end(); ++it)
+    for (AVL_INT_INT::iterator it = avlTree.begin(); it != avlTree.end(); ++it)
         std::cout << *it << std::endl;
     std::cout << "------------------------------------" << std::endl;
 
     std::cout << "------------------------------------" << std::endl;
-      for (AVL_INT_INT::reverse_iterator r_it = avlTree.rbegin(); r_it != avlTree.rend(); ++r_it)
+    for (AVL_INT_INT::reverse_iterator r_it = avlTree.rbegin(); r_it != avlTree.rend(); ++r_it)
         std::cout << *r_it << std::endl;
     std::cout << "------------------------------------" << std::endl;
 
+    {
 
-    return 0;
-}
+#endif
+
+        /*
+
+  2          2            4
+ / \          \          / \
+1   4    =>    4    =>  2   5
+   / \        / \        \
+  3   5      3   5        3
+
+*/
+        typedef ft::AVLTree<ft::pair<int, int> > AVL_INT_INT;
+        {
+            std::cout << BOLDYELLOW << "**------------------------ Delete ------------------------**" << RESET << std::endl;
+            std::cout << GREEN << "Case1: " << RESET << std::endl;
+            AVL_INT_INT avl1;
+            int case1[] = {1, 2, 3, 4, 5};
+
+            insert(avl1, case1, 5);
+
+            avl1.remove(ft::make_pair(1, 2));
+
+            put(avl1.begin(), avl1.end());
+
+            std::cout << "\t------------------------------------------" << std::endl;
+
+            put(avl1.rbegin(), avl1.rend());
+        }
+
+        return 0;
+    }
