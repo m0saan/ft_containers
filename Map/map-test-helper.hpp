5651b2dd23910b8b86db2c91461340ee3c31eec0
mo-san99 <moboustta6@gmail.com>
2021-10-13 11:55:46 +0100

    map ok!

---
+++ Map/map-test-helper.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+bool fncomp(char lhs, char rhs) { return lhs < rhs; }
+
+struct classcomp
+{
+    bool operator()(const char &lhs, const char &rhs) const
+    {
+        return lhs < rhs;
+    }
+};
